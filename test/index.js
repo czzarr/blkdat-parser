3d2ac5b6efea66227b5a804e80c2d90592ad4f58
Stanislas Marion <stanislas.marion@gmail.com>
2014-11-13 14:55:40 +0100

    1.2.0: update deps

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
+var async = require('async')
+var bl = require('bcoin-block')
+var btx = require('bcoin-tx')
 var CombinedStream = require('combined-stream')
 var fs = require('fs')
 var glob = require('glob')
@@ -5,19 +8,40 @@ var parseBlock = require('bitcoin-block-streaming-parser')
 var parseBlkDat = require('../')
 var path = require('path')
 var through = require('through2')
+var utils = require('bcoin-utils')
+var util = require('util')
 
 var dataDir = '/Users/stan/Library/Application Support/Bitcoin/testnet3/blocks'
 
 glob('blk*.dat', { cwd: dataDir }, function (err, files) {
   var cs = CombinedStream.create()
-  for (var j = 0; j < files.length; j++) {
-    cs.append(fs.createReadStream(path.resolve(dataDir, files[j])))
-  }
-  cs
-    .pipe(parseBlkDat('testnet3'))
-    .pipe(parseBlock('testnet3'))
-    .pipe(through.obj(function (chunk, enc, done) {
-      console.log(chunk);
+  async.eachSeries(
+    files,
+    function (file, done) {
+      cs.append(function (next) {
+        next(fs.createReadStream(path.resolve(dataDir, file), { mode: 0444 }))
+      })
       done()
-    }))
+    },
+    function (err) {
+      if (err)
+        throw err
+      console.log('all good');
+      cs
+        .pipe(parseBlkDat('testnet3'))
+        .pipe(parseBlock('testnet3'))
+        .pipe(through.obj(function (chunk, enc, done) {
+          var b = bl('testnet3', chunk, 'block')
+          var t = btx('testnet3', chunk.txs[0])
+          //console.log(t);
+          //console.log(util.inspect(t.outputs, false, null));
+          //console.log(t.hash('hex'));
+          //console.log(b.tx);
+          //console.log('hash', t.hash('hex'));
+          //console.log('txid', t.txid('hex'));
+          throw Error()
+          done()
+        }))
+    }
+  )
 })
